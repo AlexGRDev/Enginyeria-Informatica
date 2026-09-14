47fe968d15341d921d2d214ad03f89a65e6bd789
Alex Garcia Rodriguez <110727609+AlexGRDev@users.noreply.github.com>
2026-09-14 12:19:04 +0200

    feat(P_): adings solutions for: P51126, P56559, P89265

---
+++ 1r/Q1/PRO1/ejercicios/P_/P89265/P89265.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   P89265.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/14 07:14:52 by agarcia2          #+#    #+#             */
+/*   Updated: 2026/09/14 08:55:27 by agarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include <iostream>
+
+int	ft_min(int a, int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+int	ft_max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+int	*ft_range(int **arry1, int **arry2)
+{
+	int	min;
+	int	max;
+	int	*result;
+
+	min = ft_max(*(*arry1), *(*arry2));
+	max = ft_min(*(*arry1 + 1), *(*arry2 + 1));
+	result = (int *)malloc(sizeof(int) * 2);
+	result[0] = min;
+	result[1] = max;
+	if (result[0] > result[1])
+	{
+		std::cout << "? " << ", " << "[" << "]" << std::endl;
+		free(result);
+		return (NULL);
+	}
+	return (result);
+}
+
+int	main(void)
+{
+	int	*arry1;
+	int	*arry2;
+	int	*range;
+
+	arry1 = (int *)malloc(sizeof(int) * 2);
+	arry2 = (int *)malloc(sizeof(int) * 2);
+	if (std::cin >> arry1[0] >> arry1[1] >> arry2[0] >> arry2[1])
+	{
+		range = ft_range(&arry1, &arry2);
+		if (range != NULL)
+		{
+			if (range[0] == arry1[0] && range[1] == arry1[1] && range[0] == arry2[0] && range[1] == arry2[1])
+				std::cout << "= " << ", " << "[" << range[0] << "," << range[1] << "]" << std::endl;
+			else if (range[0] == arry1[0] && range[1] == arry1[1])
+				std::cout << "1 " << ", " << "[" << range[0] << "," << range[1] << "]" << std::endl;
+			else if (range[0] == arry2[0] && range[1] == arry2[1])
+				std::cout << "2 " << ", " << "[" << range[0] << "," << range[1] << "]" << std::endl;
+			else
+				std::cout << "? " << ", " << "[" << range[0] << "," << range[1] << "]" << std::endl;
+			free(range);
+			range = nullptr;
+		}
+	}
+	free(arry1);
+	free(arry2);
+	arry1 = nullptr;
+	arry2 = nullptr;
+	return (0);
+}
