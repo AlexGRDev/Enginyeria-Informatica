bfdd88df5b31dca736c1c5bcce8946ba318b0d0c
Alex Garcia Rodriguez <110727609+AlexGRDev@users.noreply.github.com>
2026-09-21 14:24:40 +0200

    fix(P70955): afegeix el camp de segons al càlcul de temps total
    ft_getSeconds i main passen d'una entrada de 4 valors (anys, dies,
    hores, minuts) a 5, afegint segons amb multiplicador 1.

--- 1r/Q1/PRO1/ejercicios/P_/P70955/P70955.cpp
+++ 1r/Q1/PRO1/ejercicios/P_/P70955/P70955.cpp
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/16 17:39:47 by agarcia2          #+#    #+#             */
-/*   Updated: 2026/09/16 22:34:58 by agarcia2         ###   ########.fr       */
+/*   Updated: 2026/09/21 11:52:38 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,17 @@ int	*ft_getSeconds(int **arry)
 	int	*total;
 
 	total = (int *)malloc(sizeof(int));
-	multipliers = (int *)malloc(sizeof(int) * 4);
+	multipliers = (int *)malloc(sizeof(int) * 5);
 	if (!total || !multipliers)
 		return (nullptr);
 	multipliers[0] = 365 * 24 * 3600;
 	multipliers[1] = 24 * 3600;
 	multipliers[2] = 3600;
 	multipliers[3] = 60;
+	multipliers[4] = 1;
 	*total = 0;
 	parry = *arry;
-	end = multipliers + 4;
+	end = multipliers + 5;
 	while (multipliers < end)
 	{
 		*total += *parry * *multipliers;
@@ -45,10 +46,10 @@ int	main(void)
 	int	*a;
 	int	*result;
 
-	a = (int *)malloc(sizeof(int) * 4);
+	a = (int *)malloc(sizeof(int) * 5);
 	if (!a)
 		return (1);
-	if (std::cin >> a[0] >> a[1] >> a[2] >> a[3])
+	if (std::cin >> a[0] >> a[1] >> a[2] >> a[3] >> a[4])
 	{
 		result = ft_getSeconds(&a);
 		if (result != nullptr)
